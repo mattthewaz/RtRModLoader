3f2e4a3efa925048f6b933b14a431e056ea709dd
MatthewCole Waschezyn <mattthewaz@gmail.com>
2026-05-05 03:13:38 -0400

    No longer include version number in output jar

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd "$(dirname "$0")"
-mkdir -p "$(pwd)/../output"
-OUT="$(realpath "$(pwd)/../output")"
+OUT="$(realpath "$(pwd)/../RtR")"
 cp target/RtRModLoader.jar "$OUT/"
 echo "[RtRModLoader] Deployed to $OUT"
