2abf18a50a23898cf1c2f172098b9839fc4a9a03
MatthewCole Waschezyn <mattthewaz@gmail.com>
2026-05-06 00:47:50 -0400

    Small tweak to use zip

--- package.sh
+++ package.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 cd "$(dirname "$0")"
 mvn package
-cp launch-modloader.sh launch-modloader.command launch-modloader.bat target/
-jar --create --file target/RtRModLoader.zip \
-    -C target RtRModLoader.jar \
-    -C target launch-modloader.sh \
-    -C target launch-modloader.command \
-    -C target launch-modloader.bat
+zip -j target/RtRModLoader.zip \
+    target/RtRModLoader.jar \
+    launch-modloader.sh \
+    launch-modloader.command \
+    launch-modloader.bat
 echo "[RtRModLoader] Packaged to target/RtRModLoader.zip"
